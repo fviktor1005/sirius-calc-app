4bec6265512c4401f4da256a229a9229d9d737f0
fviktor <fviktor1005@gmail.com>
2018-04-12 21:11:35 +0300

    openshift config

--- app/server/index.js
+++ app/server/index.js
@@ -1,6 +1,9 @@
 import app from './app';
 
-const port = process.env.PORT || 80;
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 3000);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost');
 
-app.listen(port);
-console.log(`Listening at http://localhost:${port}`);
+app.listen(app.get('port'), app.get('ip'), function(){
+    console.log("Express server listening on " + app.get('ip') + ":" + app.get('port'));
+});
+
