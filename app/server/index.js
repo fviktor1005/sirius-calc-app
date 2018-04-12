b7073401ef55e71033e3ca82e8fb8958ff406c99
fviktor <fviktor1005@gmail.com>
2018-04-12 23:16:31 +0300

    0000

--- app/server/index.js
+++ app/server/index.js
@@ -1,7 +1,7 @@
 import app from './app';
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || '127.0.0.1');
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || '0.0.0.0');
 
 app.listen(app.get('port'), app.get('ip'), function(){
     console.log("Express server listening on " + app.get('ip') + ":" + app.get('port'));
